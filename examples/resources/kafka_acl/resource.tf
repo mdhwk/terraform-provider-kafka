1a9091c78920aecaf81c7d985348a03c267f7b1e
mdhwk <martin@birdlabs.se>
2023-05-12 16:17:46 +0200

    fix: render docs

--- examples/resources/kafka_acl/resource.tf
+++ examples/resources/kafka_acl/resource.tf
@@ -2,7 +2,7 @@ resource "kafka_acl" "example" {
   resource_name                = "<topic_name>"
   resource_type                = "Topic"
   resource_pattern_type_filter = "Prefixed"
-  acl_principal = "CN=example_user"
-  acl_operation = "Create"
-  acl_host = "*"
+  acl_principal                = "CN=example_user"
+  acl_operation                = "Create"
+  acl_host                     = "*"
 }
